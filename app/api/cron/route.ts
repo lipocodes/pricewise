771c70b8455e9c05d655448b938d49f1b383714f
lipocodes <lipocodes@yahoo.com>
2024-01-02 17:41:10 +0200

    end

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -27,7 +27,7 @@ export async function GET(request: Request) {
     if (!products) throw new Error("No product fetched");
 
     // ======================== 1 SCRAPE LATEST PRODUCT DETAILS & UPDATE DB
-    /* const updatedProducts = await Promise.all(
+     const updatedProducts = await Promise.all(
       products.map(async (currentProduct) => {
         // Scrape product
         const scrapedProduct = await scrapeAmazonProduct(currentProduct.url);
@@ -56,7 +56,7 @@ export async function GET(request: Request) {
           },
           product
         );
-
+/*
         // ======================== 2 CHECK EACH PRODUCT'S STATUS & SEND EMAIL ACCORDINGLY
         const emailNotifType = getEmailNotifType(
           scrapedProduct,
