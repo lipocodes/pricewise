88554bdd29310fc8fa4a888837ca112509821807
lipocodes <lipocodes@yahoo.com>
2024-01-02 16:09:26 +0200

    end

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -13,7 +13,7 @@ import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 import { connectToDB } from "@/lib/actions/mongoose";
 import { scrapeAmazonProduct } from "@/lib/actions/scraper";
 
-export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const maxDuration = 10; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
