6599ff4f940783bd08d244da8059226cbe8987d0
lipocodes <lipocodes@yahoo.com>
2024-01-03 07:19:58 +0200

    end

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -34,13 +34,13 @@ export async function GET(request: Request) {
 
         if (!scrapedProduct) return;
 
-        const updatedPriceHistory = [
+        /*const updatedPriceHistory = [
           ...currentProduct.priceHistory,
           {
             price: scrapedProduct.currentPrice,
           },
         ];
-        /*
+        
         const product = {
           ...scrapedProduct,
           priceHistory: updatedPriceHistory,
